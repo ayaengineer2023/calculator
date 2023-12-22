26b93eab6d9d2c465aa72bf22037ed64d12e7978
Aya <aya.bouzi@yahoo.com>
2023-12-22 08:43:26 +0100

    Ajouter la logique de l’addition, controller et test unitaire

--- src/main/java/com/bouznari/Calculator/CalculatorApplication.java
+++ src/main/java/com/bouznari/Calculator/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package com.bouznari.calculator;
+package com.bouznari.Calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
